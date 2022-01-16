b8dacef78689d3f782ce5bbcfe1de1b41a430f65
Rana AL-HEK <rana.al-hek@isae.edu.lb>
2022-01-16 14:13:06 +0200

    TP3 Solution

--- tp3/question3/UneUtilisation.java
+++ tp3/question3/UneUtilisation.java
@@ -5,9 +5,8 @@ import question1.PolygoneRegulier;
 public class UneUtilisation {
 
     public static void main(String[] args) throws Exception {
-        // déclarer p1
-        // déclarer p2
-
+        PileI<PolygoneRegulier> p1 = new Pile2<PolygoneRegulier>();
+        PileI<Object> p2 = new Pile2<Object>();
         // p1 est ici une pile de polygones réguliers PolygoneRegulier.java
         p1.empiler(new PolygoneRegulier(4, 100));
         p1.empiler(new PolygoneRegulier(5, 100));
@@ -18,11 +17,10 @@ public class UneUtilisation {
         System.out.println(" la pile p2 = " + p2);
 
         try {
-            // p1.empiler(new PolygoneRegulier(5,100)); // désormais une erreur de
-            // compilation
-            // ....
-            // String s = (String)p1.depiler(); // désormais une erreur de
-            // compilation
+           //p1.empiler(new PolygoneRegulier(5,100)); 
+        
+       //    String s = (String)p1.depiler(); 
+       
         } catch (Exception e) {
             e.printStackTrace();
         }
